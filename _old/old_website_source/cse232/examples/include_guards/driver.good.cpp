a1ec4042aa5b2641be183208639a035f684db3b8
Kenneth Hanson <khanson679@gmail.com>
2015-08-11 15:38:48 +0900

    Removed _old.

--- _old/old_website_source/cse232/examples/include_guards/driver.good.cpp
+++
@@ -1,6 +0,0 @@
-/* User */
-
-#include “a.good.h”
-#include “b.good.h” //no error this time
-
-int main() {return 0;}
--- _old/old_website_source/cse232/fs12/examples/include_guards/_vti_cnf/driver.bad.cpp
+++
@@ -1,8 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|17 Jun 2013 03:09:25 -0000
-vti_extenderversion:SR|12.0.0.0
-vti_cacheddtm:TX|17 Jun 2013 03:09:25 -0000
-vti_filesize:IR|97
-vti_backlinkinfo:VX|cse232/fs12/examples/include_guards/include_guards.html
-vti_syncwith_afs.msu.edu\:21/web:TX|17 Jun 2013 03:09:25 -0000
-vti_syncofs_afs.msu.edu\:21/web:TW|12 Sep 2013 02:40:51 -0000
